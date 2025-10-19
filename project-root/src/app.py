5c38b68f871a9ff52371df628262ec725114c883
RomyUP1411 <rj.tipactic@alum.up.edu.pe>
2025-10-19 17:47:36 -0500

    C

---
+++ project-root/src/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Read the CSV file
+df = pd.read_csv(r"RomyUP1411/Sidpol-Peru-Crime-Dashboard-Programacion-Avanzada-G1/project-root/data/DATASET_Denuncias_Policiales_Enero 2018 a Setiembre 2025.csv")
+
+# Display the first few rows of the dataframe
+print(df.head())
